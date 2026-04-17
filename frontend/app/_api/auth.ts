3cd6f0d2a47ca3f641804f862e88107dcd6a1dbc
Sonny Pimentel <spimentelm1201@gmail.com>
2026-04-17 12:36:41 -0500

    Update API base URL for authentication
    Signed-off-by: Sonny Pimentel <spimentelm1201@gmail.com>

--- frontend/app/_api/auth.ts
+++ frontend/app/_api/auth.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const BASE_URL = 'https://vetcare-qwzz.onrender.com/api/v1'
+const BASE_URL = 'https://vetcare-5y9z.onrender.com/api/v1'
 // const BASE_URL = 'localhost:3001/api/v1'
 
 const api = axios.create({
